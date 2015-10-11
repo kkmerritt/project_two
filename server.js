dec477b7dd2e17bd789240d345fc10caabbb246c
Kevin Merritt <kkmerritt@gmail.com>
2015-10-11 19:22:26 -0400

    index server basics

--- server.js
+++ server.js
@@ -2,15 +2,19 @@
 // NOTE: ----------------------  This is the server.
 
 var express    = require('express'),
-  server     = express(),
   ejs        = require('ejs'),
   bodyParser = require('body-parser'),
-  mongoose   = require('mongoose'),
   methodOverride = require('method-override'),
   // expressLayouts = require('express-ejs-layouts'),
-  dbURL      = 'mongodb://localhost:27017/users',
   Schema     = mongoose.Schema,
   morgan     = require('morgan');
+
+  PORT = process.env.PORT || 6667,
+  server = express(),
+  MONGOURI = process.env.MONGOLAB_URI || "mongodb://localhost:27017",
+  dbname = "project_two_db"
+  mongoose = require('mongoose');
+
 
 server.set('views', './views'); // tells the renderer where to find templates
 server.set('view engine', 'ejs'); // tells the render method, what to use
@@ -19,13 +23,7 @@ server.use(express.static(__dirname + '/public')); //location for static files (
 server.use(bodyParser.urlencoded({extended: true})); // So we can parse incoming forms into Objects
 server.use(methodOverride('_method'));
 server.use(morgan('short'));
-// server.use(expressLayouts);
 
-PORT = process.env.PORT || 6667,
-server = express(),
-MONGOURI = process.env.MONGOLAB_URI || "mongodb://localhost:27017",
-dbname = "project_two_db"
-mongoose = require('mongoose');
 
 
 // NOTE: ---------------------- Server Functions
