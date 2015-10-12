7b55d5ed5d1b2478e2bf370a8d8b8db6c8ec5164
Kevin Merritt <kkmerritt@gmail.com>
2015-10-12 15:43:41 -0400

    redesign, simplified, response. basic content loaded. functioning on localhost

--- server.js
+++ server.js
@@ -1,21 +1,20 @@
-// NOTE: ----------------------  Kevin Merritt, Post Poster
+// NOTE: ----------------------  Kevin Merritt, Forum Project
 // NOTE: ----------------------  This is the server.
-
 
 
 var express    = require('express'),
   ejs        = require('ejs'),
   bodyParser = require('body-parser'),
   methodOverride = require('method-override'),
-  // expressLayouts = require('express-ejs-layouts'),
+  expressLayouts = require('express-ejs-layouts'),
   // Schema     = mongoose.Schema,
   morgan     = require('morgan');
 
-  PORT = process.env.PORT || 6667,
-  server = express(),
+  PORT = process.env.PORT || 3000,
+    server = express(),
   MONGOURI = process.env.MONGOLAB_URI || "mongodb://localhost:27017",
-  dbname = "project_two_db"
-  mongoose = require('mongoose');
+    dbname = "project_two_db"
+    mongoose = require('mongoose');
 
 
 server.set('views', './views'); // tells the renderer where to find templates
@@ -25,6 +24,7 @@ server.use(express.static(__dirname + '/public')); //location for static files (
 server.use(bodyParser.urlencoded({extended: true})); // So we can parse incoming forms into Objects
 server.use(methodOverride('_method'));
 server.use(morgan('short'));
+server.use(expressLayouts);
 
 
 
@@ -33,6 +33,16 @@ server.get('/', function (req, res) {
   res.render('index')
 });
 
+server.get('/submit', function (req, res) {
+  res.render('submit')
+});
+
+server.get('/user', function (req, res) {
+  res.render('user')
+});
+server.get('/contact', function (req, res) {
+  res.render('contact')
+});
 
 mongoose.connect(MONGOURI + "/" + dbname)
 server.listen(PORT,function(){
