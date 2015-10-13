83d6c847b407896680dfb291042b305adf4464ca
Kevin Merritt <kkmerritt@gmail.com>
2015-10-13 12:20:23 -0400

    restructuring, removed excess pages

--- server.js
+++ server.js
@@ -43,14 +43,15 @@ db.once('open', function(){
 
 // NOTE: ---------------------- DATABASE
 var Post = mongoose.model("post",{
-            author: String,
+            email: String,
             date: String,
             content: String,
+            avatar: String,
 });
 
 // NOTE this is for testing only ---------------------------------------
 // var newPost = new Post({       //test post.
-//   author: "Kevin Test",
+//   email: "Kevin Test",
 //   content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 // });
 //
@@ -83,8 +84,8 @@ server.get('/submit', function (req, res) {
   res.render('submit')
 });
 
-server.get('/user', function (req, res) {
-  res.render('user')
+server.get('/thisuser', function (req, res) {
+  res.render('thisuser')
 });
 
 server.get('/contact', function (req, res) {
